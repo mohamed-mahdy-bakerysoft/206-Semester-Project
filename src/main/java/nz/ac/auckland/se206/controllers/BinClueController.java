77e5309e62bb7774f45bf65942604c929943f561
kimkimz <kimberleymzhu@gmail.com>
2024-09-17 11:30:25 +1200

    added a zoom into the ID card when the player finds it in the bin

--- src/main/java/nz/ac/auckland/se206/controllers/BinClueController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/BinClueController.java
@@ -1,9 +1,12 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.apiproxy.exceptions.ApiProxyException;
 import nz.ac.auckland.se206.DraggableMaker;
 
@@ -17,6 +20,8 @@ public class BinClueController extends ClueController {
   @FXML private ImageView Rubbish4;
   @FXML private ImageView Rubbish5;
   @FXML private ImageView Keycard;
+  @FXML private ImageView Keycard2;
+  @FXML private Rectangle rectBackDrop;
 
   DraggableMaker draggableMaker = new DraggableMaker();
 
@@ -28,6 +33,15 @@ public class BinClueController extends ClueController {
     draggableMaker.makeDraggable(Rubbish3);
     draggableMaker.makeDraggable(Rubbish4);
     draggableMaker.makeDraggable(Rubbish5);
-    draggableMaker.makeDraggable(Keycard);
+  }
+
+  public void handleKeyCard(MouseEvent event) throws IOException {
+    rectBackDrop.setVisible(true);
+    Keycard2.setVisible(true);
+  }
+
+  public void handleClickBack(MouseEvent event) throws IOException {
+    rectBackDrop.setVisible(false);
+    Keycard2.setVisible(false);
   }
 }
