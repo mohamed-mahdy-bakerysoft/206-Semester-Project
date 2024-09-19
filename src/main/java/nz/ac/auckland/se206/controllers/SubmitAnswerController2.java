6888c13fe616f2c94290d121276bd443ab99eced
kimkimz <kimberleymzhu@gmail.com>
2024-09-19 22:45:38 +1200

    added a back button so that player can choose someone else if they accidentally clicked tthe wrong file

--- src/main/java/nz/ac/auckland/se206/controllers/SubmitAnswerController2.java
+++ src/main/java/nz/ac/auckland/se206/controllers/SubmitAnswerController2.java
@@ -1,7 +1,9 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+import nz.ac.auckland.se206.App;
 
 public class SubmitAnswerController2 extends SubmitAnswerController {
   @FXML private ImageView JanitorPhoto;
@@ -22,5 +24,10 @@ public class SubmitAnswerController2 extends SubmitAnswerController {
     } else if (getThief().equals("curator")) {
       CuratorPhoto.setVisible(true);
     }
+  }
+
+  @FXML
+  private void onGoBack() throws IOException {
+    App.setRoot("whosThief");
   }
 }
