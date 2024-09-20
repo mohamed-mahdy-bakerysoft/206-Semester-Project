90807f136d37372bd682c772dd210ebba78d0cad
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-09-20 15:45:06 +1200

    Refactor: Add javadocs and comments to LogBookClueController

--- src/main/java/nz/ac/auckland/se206/controllers/LogBookClueController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LogBookClueController.java
@@ -7,6 +7,10 @@ import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.apiproxy.exceptions.ApiProxyException;
 import nz.ac.auckland.se206.App;
 
+/**
+ * The LogBookClueController class manages the interaction with the logbook clue in the game. It
+ * allows the player to turn pages in the logbook to investigate clues.
+ */
 public class LogBookClueController extends ClueController {
 
   @FXML private Rectangle rectLogBook;
@@ -15,11 +19,23 @@ public class LogBookClueController extends ClueController {
   @FXML private Rectangle rectPageTurnLeft;
   @FXML private Rectangle rectPageTurnLeft2;
 
+  /**
+   * Initializes the logbook clue view by calling the parent class's initialize method.
+   *
+   * @throws ApiProxyException if there is an error communicating with the API proxy
+   */
   @Override
   public void initialize() throws ApiProxyException {
     super.initialize();
   }
 
+  /**
+   * Handles the event when the right page turn button is clicked. It changes the view to the next
+   * logbook page.
+   *
+   * @param event the mouse event triggered by clicking a page turn button
+   * @throws IOException if there is an error loading the next page
+   */
   public void handleTurnPageClickRight(MouseEvent event) throws IOException {
     Rectangle clicked = (Rectangle) event.getSource();
     if (clicked.getId().equals("rectPageTurnRight")) {
@@ -29,13 +45,14 @@ public class LogBookClueController extends ClueController {
     }
   }
 
+  /**
+   * Handles the event when the left page turn button is clicked. It changes the view to the
+   * previous logbook page.
+   *
+   * @param event the mouse event triggered by clicking a page turn button
+   * @throws IOException if there is an error loading the previous page
+   */
   public void handleTurnPageClickLeft(MouseEvent event) throws IOException {
-    // if (context.getCurrentState()
-    //     instanceof Guessing) { // if in guessing phase, the other areas should not be accessible
-    // for
-    //   // investigation
-    //   return;
-    // }
     Rectangle clicked = (Rectangle) event.getSource();
     if (clicked.getId().equals("rectPageTurnLeft")) {
       App.setRoot("logbookclue1");
