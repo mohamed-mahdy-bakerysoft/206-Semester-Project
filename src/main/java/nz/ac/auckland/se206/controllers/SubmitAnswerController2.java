e46fcdb0fabbd3a526bf15c98a98079cb45a1185
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-09-20 15:52:09 +1200

    Refactor: Add javadocs and comments to SubmitAnswerController2

--- src/main/java/nz/ac/auckland/se206/controllers/SubmitAnswerController2.java
+++ src/main/java/nz/ac/auckland/se206/controllers/SubmitAnswerController2.java
@@ -3,16 +3,29 @@ package nz.ac.auckland.se206.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 
+/**
+ * The SubmitAnswerController2 class extends SubmitAnswerController and is responsible for
+ * displaying the chosen suspect's photo based on the player's selection.
+ */
 public class SubmitAnswerController2 extends SubmitAnswerController {
+
   @FXML private ImageView JanitorPhoto;
   @FXML private ImageView HOSPhoto;
   @FXML private ImageView CuratorPhoto;
 
+  /**
+   * Initializes the controller by calling the parent initialize method and then displaying the
+   * chosen suspect's photo.
+   */
   public void initialize() {
     super.initialize();
     displayChosenSuspect();
   }
 
+  /**
+   * Displays the photo of the chosen suspect based on the thief value. Sets the visibility of the
+   * corresponding ImageView (Janitor, HOS, or Curator).
+   */
   @FXML
   private void displayChosenSuspect() {
     if (getThief().equals("janitor")) {
