00cf13e067290b6899d9b83272560a0c5fd4264a
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-09-20 14:31:55 +1200

    Refactor: Add javadocs and comments to ComputerClueController

--- src/main/java/nz/ac/auckland/se206/controllers/ComputerClueController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/ComputerClueController.java
@@ -10,19 +10,34 @@ import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.apiproxy.exceptions.ApiProxyException;
 import nz.ac.auckland.se206.App;
 
+/**
+ * The ComputerClueController class handles interactions with the computer clue interface, such as
+ * clicking on various elements and entering a password. It extends the ClueController and manages
+ * events related to the computer clue puzzle.
+ */
 public class ComputerClueController extends ClueController {
 
+  /** Tracks the number of incorrect password guesses made by the user. */
   private static int passwordGuesses = 0;
+
+  /** Flag to indicate whether the password has been successfully guessed. */
   public static boolean passwordHasBeenGuessed = false;
 
+  /**
+   * Returns whether the password has been successfully guessed.
+   *
+   * @return true if the password has been guessed, false otherwise
+   */
   public static boolean getPasswordHasBeenGuessed() {
     return passwordHasBeenGuessed;
   }
 
+  /** Increments the number of incorrect password guesses made by the user. */
   private static void incrementPasswordGuesses() {
     passwordGuesses++;
   }
 
+  // FXML components representing various clickable elements on the computer clue scene.
   @FXML private Rectangle rectOpenEmail;
   @FXML private Rectangle rectEscape;
   @FXML private Rectangle rectBackToInbox;
@@ -31,11 +46,23 @@ public class ComputerClueController extends ClueController {
   @FXML private ImageView passwordHint;
   @FXML private Rectangle rectSecurityCamera;
 
+  /**
+   * Initializes the computer clue view, including loading any necessary assets.
+   *
+   * @throws ApiProxyException if there is an error communicating with the API proxy
+   */
   @Override
   public void initialize() throws ApiProxyException {
     super.initialize();
   }
 
+  /**
+   * Handles clicks on various elements of the computer clue interface, navigating to the next clue
+   * depending on the clicked element.
+   *
+   * @param event the mouse event triggered by clicking on a rectangle
+   * @throws IOException if there is an I/O error during scene transition
+   */
   public void handleComputerClick(MouseEvent event) throws IOException {
     Rectangle clicked = (Rectangle) event.getSource();
     switch (clicked.getId()) {
@@ -55,26 +82,41 @@ public class ComputerClueController extends ClueController {
     }
   }
 
+  /**
+   * Handles password input and checks if the entered password is correct. If the password is wrong
+   * and more than two attempts have been made, a password hint is displayed.
+   *
+   * @param event the mouse event triggered by clicking to submit the password
+   * @throws IOException if there is an I/O error during scene transition
+   */
   public void handlePassword(MouseEvent event) throws IOException {
     incrementPasswordGuesses();
     if (passwordTxtField.getText().equals("willthegoat")) {
       App.setRoot("computerclue1");
       passwordHasBeenGuessed = true;
-    } else if (passwordGuesses >= 2
-        && !passwordTxtField
-            .getText()
-            .equals("")) { // if not empty and user has guessed more than twice
+    } else if (passwordGuesses >= 2 && !passwordTxtField.getText().isEmpty()) {
       passwordTxtField.clear();
       passwordHint.setVisible(true);
     }
   }
 
+  /**
+   * Handles pressing the Enter key to submit the password. This method triggers password checking
+   * without needing a mouse click.
+   *
+   * @param keyEvent the key event triggered by pressing the Enter key
+   * @throws IOException if there is an I/O error during scene transition
+   */
   public void handleEnter(KeyEvent keyEvent) throws IOException {
     if (keyEvent.getCode().toString().equals("ENTER")) {
       handlePassword(null);
     }
   }
 
+  /**
+   * Resets the password guess count and status, allowing the puzzle to be attempted again from the
+   * beginning.
+   */
   public static void reset() {
     passwordGuesses = 0; // Reset the number of password guesses
     passwordHasBeenGuessed = false; // Reset the guessed status
