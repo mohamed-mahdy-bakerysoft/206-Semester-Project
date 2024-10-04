e06513e09e006f268016c3f42e16a7d614d3c54f
kimkimz <kimberleymzhu@gmail.com>
2024-10-04 17:26:22 +1300

    moved the static methods to the correct order

--- src/main/java/nz/ac/auckland/se206/controllers/InteragationRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/InteragationRoomController.java
@@ -126,6 +126,27 @@ public class InteragationRoomController implements RoomNavigationHandler {
     suspectHasBeenTalkedToMap.put("Janitor", false);
   }
 
+  private static void initializeRoleToNameMap() {
+    professionToNameMap.put("Art Currator", "Frank");
+    professionToNameMap.put("Art Thief", "William");
+    professionToNameMap.put("Janitor", "John");
+    professionToNameMap.put("user", "You");
+  }
+
+  public static void resetStaticVariables() {
+    clueHasBeenInteractedWith = false;
+    isFirstTimeInit = true;
+    isChatOpened = false;
+
+    // Reset suspectHasBeenTalkedToMap
+    suspectHasBeenTalkedToMap.clear();
+    initializeSuspectTalkedToMap();
+
+    // Reset professionToNameMap if necessary
+    professionToNameMap.clear();
+    initializeRoleToNameMap();
+  }
+
   @FXML private BorderPane mainPane;
   @FXML private Button corridorButton;
   @FXML private Button suspect1Button;
@@ -283,20 +304,6 @@ public class InteragationRoomController implements RoomNavigationHandler {
 
       new Thread(task).start();
     }
-  }
-
-  public static void resetStaticVariables() {
-    clueHasBeenInteractedWith = false;
-    isFirstTimeInit = true;
-    isChatOpened = false;
-
-    // Reset suspectHasBeenTalkedToMap
-    suspectHasBeenTalkedToMap.clear();
-    initializeSuspectTalkedToMap();
-
-    // Reset professionToNameMap if necessary
-    professionToNameMap.clear();
-    initializeRoleToNameMap();
   }
 
   private void sendMessageToAI(
@@ -477,13 +484,6 @@ public class InteragationRoomController implements RoomNavigationHandler {
       default:
         return "";
     }
-  }
-
-  private static void initializeRoleToNameMap() {
-    professionToNameMap.put("Art Currator", "Frank");
-    professionToNameMap.put("Art Thief", "William");
-    professionToNameMap.put("Janitor", "John");
-    professionToNameMap.put("user", "You");
   }
 
   /**
